703776af35780d1f594fea0e369fac5a731dd6f5
jujusoi <jalxmcdonald@hotmail.com>
2023-08-29 12:34:04 +0930

    started seeding

--- db/schema.sql
+++ db/schema.sql
@@ -5,20 +5,26 @@ USE employee_tracker;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    dep_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT FOREIGN KEY REFERENCES department(id),
+    department_id INT,
+    FOREIGN KEY (department_id) 
+    REFERENCES department(id)
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT FOREIGN KEY REFERENCES roles(id),
-    manager_id INT FOREIGN KEY REFERENCES employee(id),
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id),
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (dep_name)
+VALUES ('Sales'),
+       ('Engineering'),
+       ('Finance'),
+       ('Legal');
+
+SELECT * FROM department;
+
